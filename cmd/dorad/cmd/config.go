e84f61e839607e220c871e5fefe088db62b08134
VegeBun-csj <csj1071285741@gmail.com>
2023-07-05 11:29:54 +0800

    add dora decimal

--- cmd/dorad/cmd/config.go
+++ cmd/dorad/cmd/config.go
@@ -6,6 +6,15 @@ import (
 	"dora/app"
 )
 
+const (
+	// set token decimal
+	HumanCoinUnit = "DORA"
+	BaseCoinUnit  = "uDORA"
+	DoraExponent  = 6
+
+	DefaultBondDenom = BaseCoinUnit
+)
+
 func initSDKConfig() {
 	// Set prefixes
 	accountPubKeyPrefix := app.AccountAddressPrefix + "pub"
@@ -19,5 +28,15 @@ func initSDKConfig() {
 	config.SetBech32PrefixForAccount(app.AccountAddressPrefix, accountPubKeyPrefix)
 	config.SetBech32PrefixForValidator(validatorAddressPrefix, validatorPubKeyPrefix)
 	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
+
+	err := sdk.RegisterDenom(HumanCoinUnit, sdk.OneDec())
+	if err != nil {
+		panic(err)
+	}
+	err = sdk.RegisterDenom(BaseCoinUnit, sdk.NewDecWithPrec(1, DoraExponent))
+	if err != nil {
+		panic(err)
+	}
+
 	config.Seal()
 }
