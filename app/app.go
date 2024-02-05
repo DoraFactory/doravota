4a3232fd0a4b0cc9e4bf7ce97244541a1bfb7dfb
VegeBun-csj <csj1071285741@gmail.com>
2024-02-05 07:55:42 -0300

    update

--- app/app.go
+++ app/app.go
@@ -1090,19 +1090,13 @@ func (app *App) setupUpgradeHandlers() {
 		func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 
 			// logger := ctx.Logger().With("upgrade", v0_3_1.UpgradeName)
-			stakingKey := sdk.NewKVStoreKey(stakingtypes.ModuleName)
+			// stakingKey := sdk.NewKVStoreKey(stakingtypes.ModuleName)
 
 			validators := app.StakingKeeper.GetAllValidators(ctx)
 
 			for _, validator := range validators {
 				
-/* 				logger.Info("previous validator is ", validator)
-				app.StakingKeeper.SetValidatorByPowerIndex(ctx, validator)
-				logger.Info("current validator is ", validator)
-				app.StakingKeeper.ApplyAndReturnValidatorSetUpdates(ctx) */
-
-				
-				store := ctx.KVStore(stakingKey)
+				store := ctx.KVStore(app.StakingKeeper.storeKey)
 				deleted := false
 
 				iterator := sdk.KVStorePrefixIterator(store, stakingtypes.ValidatorsByPowerIndexKey)
